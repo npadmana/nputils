a719c0fe3f3c54c97aeb3d969dd396348c7188f4
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-02 22:12:25 -0400

    Improved external project build and install

--- external/install_gtest.sh
+++ external/install_gtest.sh
@@ -1,4 +1,4 @@
 mkdir -p $1/local/include
 mkdir -p $1/local/lib
-cp -R $1/build/src/gtest/include/* $1/local/include
-cp -R $1/build/src/gtest-build/*.a $1/local/lib
+cp -R $2/include/* $1/local/include
+cp -R $3/*.a $1/local/lib
