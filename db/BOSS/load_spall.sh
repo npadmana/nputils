5e3e1533b9228cfa747442248947f39966a348e6
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-06 22:25:05 -0400

    database loading scripts

---
+++ db/BOSS/load_spall.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SPALLDIR=/scratch/padmanabhan/BOSS/boss_spectro_redux
+SPALLVER=5_4_45
+
+STILTLOC=/home/np274/myWork/nputils/stilts
+STILTARGS1='omode=tosql protocol=postgresql host=devel db=sdss write=dropcreate'
+STILTUSER='user=np274 password=postgres'
+JDBC='-Djdbc.drivers=org.postgresql.Driver'
+
+
+for ii in core photo flux resolve; do
+$STILTLOC/stilts $JDBC -verbose tpipe in=$SPALLDIR/spAll-v$SPALLVER.fits dbtable=spall_v$SPALLVER.$ii cmd=@spall_$ii.txt $STILTARGS1 $STILTUSER 
+done
