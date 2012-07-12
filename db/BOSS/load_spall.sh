05e731bc142850631759b9e9ee7c7b25eadb83ef
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-12 14:59:05 -0400

    Updates to make load_spall work

--- db/BOSS/load_spall.sh
+++ db/BOSS/load_spall.sh
@@ -4,11 +4,11 @@ SPALLDIR=/scratch/padmanabhan/BOSS/boss_spectro_redux
 SPALLVER=5_4_45
 
 STILTLOC=/home/np274/myWork/nputils/stilts
-STILTARGS1='omode=tosql protocol=postgresql host=devel db=sdss write=dropcreate'
+STILTARGS1='omode=tosql protocol=postgresql host=localhost db=sdss write=dropcreate'
 STILTUSER='user=np274 password=postgres'
 JDBC='-Djdbc.drivers=org.postgresql.Driver'
 
 
 for ii in core photo flux resolve; do
-$STILTLOC/stilts $JDBC -verbose tpipe in=$SPALLDIR/spAll-v$SPALLVER.fits dbtable=spall_v$SPALLVER.$ii cmd=@spall_$ii.txt $STILTARGS1 $STILTUSER 
+$STILTLOC/stilts $JDBC -Xmx8G  -verbose tpipe in=$SPALLDIR/spAll-v$SPALLVER.fits dbtable=spall_v$SPALLVER.$ii cmd=@spall_$ii.txt $STILTARGS1 $STILTUSER 
 done
