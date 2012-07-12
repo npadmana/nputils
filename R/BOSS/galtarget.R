f3bfe66681cfbab3ffd9066599440faef2c900fd
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-12 18:40:21 -0400

    Pull out some Flagname2val and Flux2Mag into common, and add in AuxColor computation

--- R/BOSS/galtarget.R
+++ R/BOSS/galtarget.R
@@ -1,14 +1,28 @@
 # Specify the BOSS_TARGET1 galaxy flags; these are all in the lower 32 bits
 # so we don't need to worry about the lack of a native 64 bit int in R
-bosstarget1 <- list()
-bosstarget1[["GAL_LOZ"]] <- 0L
-bosstarget1[["GAL_CMASS"]] <- 1L
-bosstarget1[["GAL_CMASS_COMM"]] <- 2L
-bosstarget1[["GAL_CMASS_SPARSE"]] <- 3L
-bosstarget1[["SDSS_GAL_KNOWN"]] <- 6L
-bosstarget1[["GAL_CMASS_ALL"]] <- 7L
+boss.target1 <- list()
+boss.target1[["GAL_LOZ"]] <- 0L
+boss.target1[["GAL_CMASS"]] <- 1L
+boss.target1[["GAL_CMASS_COMM"]] <- 2L
+boss.target1[["GAL_CMASS_SPARSE"]] <- 3L
+boss.target1[["SDSS_GAL_KNOWN"]] <- 6L
+boss.target1[["GAL_CMASS_ALL"]] <- 7L
 
-boss_flagname2val <- function (names, flagdefs) {
-  tmpfun <- function(name1) return(flagdefs[[name1]])
-  sum(2L^sapply(names, tmpfun))
+
+boss.ComputeAuxColors <- function(data) {
+  # Computes the BOSS galaxy TS auxiliary colors cperp, cpar and dperp
+  #
+  # Args :
+  #   data : a data frame with MODELFLUX_[2,3,4] and EXTINCTION_[2,3,4] 
+  #      The naming conventions come from STILTS loading an SQL database
+  #
+  # Returns :
+  #   A modified data frame with cperp, cpar, and dperp. 
+  g <- boss.Flux2Mag(data$MODELFLUX_2) - data$EXTINCTION_2
+  r <- boss.Flux2Mag(data$MODELFLUX_3) - data$EXTINCTION_3
+  i <- boss.Flux2Mag(data$MODELFLUX_4) - data$EXTINCTION_4
+  dperp <- (r-i) - (g-r)/8.0
+  cperp <- (r-i) - (g-r)/4.0 - 0.18
+  cpar <- 0.7*(g-r) + 1.2*(r-i-0.18)
+  return(cbind(data, g, r, i))
 }
