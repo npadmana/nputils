17acd77cf0336e5a015a748a6a22d7c86279042f
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-07-18 13:05:46 -0400

    More refactoring and complete TS implemented

--- R/BOSS/galtarget.R
+++ R/BOSS/galtarget.R
@@ -27,6 +27,7 @@ boss.ComputeAuxColors <- function(data) {
     g <- boss.Flux2Mag(MODELFLUX_2) - EXTINCTION_2
     r <- boss.Flux2Mag(MODELFLUX_3) - EXTINCTION_3
     i <- boss.Flux2Mag(MODELFLUX_4) - EXTINCTION_4
+    z <- boss.Flux2Mag(MODELFLUX_5) - EXTINCTION_5
     gr <- g-r
     ri <- r-i
     dperp <- (ri) - (gr)/8.0
@@ -36,10 +37,14 @@ boss.ComputeAuxColors <- function(data) {
     # Fluxes and magnitudes
     rcmodel <- boss.Flux2Mag(CMODELFLUX_3) - EXTINCTION_3
     icmodel <- boss.Flux2Mag(CMODELFLUX_4) - EXTINCTION_4
+    rpsf <- boss.Flux2Mag(PSFFLUX_3) - EXTINCTION_3
+    ipsf <- boss.Flux2Mag(PSFFLUX_4) - EXTINCTION_4
+    zpsf <- boss.Flux2Mag(PSFFLUX_5) - EXTINCTION_5
+    ifib2 <- boss.Flux2Mag(FIBER2FLUX_4) - EXTINCTION_4
   })
 }
 
-boss.SelectCMASS <- function(data) {
+boss.SelectCMASS_Helper <- function(data, intercept=19.86) {
   # Selects CMASS objects and adds in a new column in the dataframe : is.CMASS
   #
   # Args :
@@ -50,7 +55,22 @@ boss.SelectCMASS <- function(data) {
   #    A new data frames with is.CMASS appended to it.
   #
   # TODO : ONLY THE SLIDING CMASS CUT HAS BEEN IMPLEMENTED
-  data[, is.CMASS := icmodel < (19.86 + 1.6*(dperp-0.8))]
+  with(data, {
+    is.CMASS <- icmodel < (intercept + 1.6*(dperp-0.8))
+    is.CMASS <- is.CMASS & (dperp > 0.55)
+    is.CMASS <- is.CMASS & (icmodel > 17.5) & (icmodel < 19.9)
+    is.CMASS <- is.CMASS & ((ipsf-i) > (0.2 + 0.2*(20.0-i)))
+    is.CMASS <- is.CMASS & ((zpsf-z) > 9.125*0.46*z)
+    is.CMASS <- is.CMASS & (ri < 2)
+    is.CMASS <- is.CMASS & (ifib2 < 21.5)
+    # Eliminate NA
+    is.CMASS[!is.finite(is.CMASS)] <- FALSE
+    return(is.CMASS)
+  })
+}
+
+boss.SelectCMASS <- function(data) {
+  data[, is.CMASS := boss.SelectCMASS_Helper(data)]
 }
 
 boss.SelectLOWZ <- function(data) {
@@ -64,8 +84,14 @@ boss.SelectLOWZ <- function(data) {
   #    A new data frames with is.LOWZ appended to it.
   #
   # TODO : ONLY THE CPERP AND CPAR CUTS HAVE BEEN IMPLEMENTED
-  data[, is.LOWZ := rcmodel < (13.5 + cpar/0.3)]
-  data[, is.LOWZ := is.LOWZ & (abs(cperp) < 0.2)]
+  within(data, {
+    is.LOWZ <- rcmodel < (13.5 + cpar/0.3)
+    is.LOWZ <- is.LOWZ & (abs(cperp) < 0.2)
+    is.LOWZ <- is.LOWZ & (rcmodel > 16) & (rcmodel < 19.6)
+    is.LOWZ <- is.LOWZ & ((rpsf - rcmodel) > 0.3)
+    # Eliminate NA
+    is.LOWZ[!is.finite(is.LOWZ)] <- FALSE
+  })
 }
 
 boss.SelectSPARSE <- function(data) {
@@ -79,8 +105,10 @@ boss.SelectSPARSE <- function(data) {
   #    A new data frames with is.SPARSE appended to it.
   #
   # TODO : ONLY THE SLIDING CUT HAS BEEN IMPLEMENTED
-  data[, is.SPARSE := icmodel >= (19.86 + 1.6*(dperp-0.8))]
-  data[, is.SPARSE := is.SPARSE & (icmodel < (20.14 + 1.6*(dperp-0.8)))]
+  if (!("is.CMASS" %in names(data))) {
+    data <- boss.SelectCMASS(data)
+  }
+  data[, is.SPARSE := !(is.CMASS) & boss.SelectCMASS_Helper(data, intercept=20.14)]
 }
 
 boss.galTypes <- list()
