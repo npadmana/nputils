6941ab97856b11eb35bb8d3f996af513b7c31abb
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-09-21 09:32:49 -0400

    Use a config file, write out to file

--- codes/fastRR/shellRR.cpp
+++ codes/fastRR/shellRR.cpp
@@ -5,6 +5,9 @@
 
 #include <iostream>
 #include <vector>
+#include <tuple>
+#include <string>
+#include <fstream>
 #include <Eigen/Core>
 #include <cmath>
 #include "boost/program_options.hpp"
@@ -20,6 +23,7 @@ using boost::format;
 namespace po = boost::program_options;
 
 typedef vector<Vector3d> Pos;
+typedef tuple<double, double> paird;
 
 Pos generate(int npart, double rmin, double rmax, unsigned long int seed) {
 	Pos out;
@@ -38,65 +42,108 @@ Pos generate(int npart, double rmin, double rmax, unsigned long int seed) {
 	return out;
 }
 
-void compute_rmu(Vector3d x, Vector3d y, double &rr, double &mu) {
+void compute_rmu(const Vector3d& x, const Vector3d& y, double &rr, double &mu) {
+	const double eps=1.e-12; // Avoid divide by zero
+
 	Vector3d s, l;
 	s = x-y;
 	l = (x+y)/2.0;
 	rr = s.norm();
-	mu = l.dot(s)/(l.norm()*rr);
+	mu = l.dot(s)/(l.norm()*(rr+eps));
 }
 
 int main(int argc, char** argv) {
 
-	int nR, nrbins, nmubins;
+
+	int nR, nrbins, nmubins, nsims;
 	unsigned long int seed;
 	double rmin, rmax, r0, r1, mu0, mu1;
+	string outfn;
 
 	// Get the input parameters -- pull them into their own scope
 	{
-		po::options_description desc("Allowed options");
-		desc.add_options()
-	    		("help", "produce help message")
-	    		("nR", po::value<int>(&nR)->default_value(100), "Number of R points")
-	    		("rmin", po::value<double>(&rmin)->default_value(500.0), "Minimum r value for shell")
-	    		("rmax", po::value<double>(&rmax)->default_value(2000.0), "Maximum r value for shell")
-	    		("r0", po::value<double>(&r0)->default_value(10.0), "Minimum r value for histogram")
-	    		("r1", po::value<double>(&r1)->default_value(100.0), "Maximum r value for histogram")
-	    		("mu0", po::value<double>(&mu0)->default_value(-1.0), "Minimum mu value for histogram")
-	    		("mu1", po::value<double>(&mu1)->default_value(1.0), "Maximum mu value for histogram")
-	    		("nrbins", po::value<int>(&nrbins)->default_value(30), "Number of r bins")
-	    		("nmubins", po::value<int>(&nmubins)->default_value(200), "Number of mu bins")
-	    		("seed", po::value<unsigned long int>(&seed)->default_value(99), "Seed value")
-	    		;
+		try {
+			string config_fn;
 
-		po::variables_map vm;
-		po::store(po::parse_command_line(argc, argv, desc), vm);
-		po::notify(vm);
+			po::options_description desc("Allowed options");
+			desc.add_options()
+	    				("help", "produce help message")
+	    				("config", po::value<string>(&config_fn), "Configuration filename")
+	    				("nR", po::value<int>(&nR)->default_value(100), "Number of R points")
+	    				("rmin", po::value<double>(&rmin)->default_value(500.0), "Minimum r value for shell")
+	    				("rmax", po::value<double>(&rmax)->default_value(2000.0), "Maximum r value for shell")
+	    				("r0", po::value<double>(&r0)->default_value(10.0), "Minimum r value for histogram")
+	    				("r1", po::value<double>(&r1)->default_value(100.0), "Maximum r value for histogram")
+	    				("mu0", po::value<double>(&mu0)->default_value(-1.0), "Minimum mu value for histogram")
+	    				("mu1", po::value<double>(&mu1)->default_value(1.0), "Maximum mu value for histogram")
+	    				("nrbins", po::value<int>(&nrbins)->default_value(30), "Number of r bins")
+	    				("nmubins", po::value<int>(&nmubins)->default_value(200), "Number of mu bins")
+	    				("seed", po::value<unsigned long int>(&seed)->default_value(99), "Seed value")
+	    				("nsims", po::value<int>(&nsims)->default_value(1), "Number of simulations")
+	    				("output",po::value<string>(&outfn)->default_value("shellRR.out"), "Output file")
+	    				;
 
-		if (vm.count("help")) {
-			cout << desc << "\n";
+			po::variables_map vm;
+			po::store(po::parse_command_line(argc, argv, desc), vm);
+			po::notify(vm);
+
+			if (vm.count("help")) {
+				cout << desc << "\n";
+				return 1;
+			}
+
+			if (vm.count("config")) {
+				ifstream ifs(config_fn.c_str());
+				if (!ifs) {
+					cout << "Error opening configuration file " << config_fn << endl;
+					return 1;
+				}
+				po::store(po::parse_config_file(ifs, desc), vm);
+				po::notify(vm);
+				ifs.close();
+			}
+		}
+		catch (exception &e) {
+			cout << e.what() << "\n";
 			return 1;
 		}
 
 	}
 
-	cout << format("# Using %1% random points from r=%2% to %3%\n") % nR % rmin % rmax;
-	cout << format("# Histogramming r from %1% to %2% in %3% bins\n") % r0 % r1 % nrbins;
-	cout << format("# Histogramming mu from %1% to %2% in %3% bins\n") % mu0 % mu1 % nmubins;
-	cout << "# Generating points...\n";
-	Pos pp = generate(nR, rmin, rmax, seed);
-	cout << "# Points generated...\n";
+	ofstream ofs(outfn);
+	if (!ofs) {
+		cout << "Unable to open output file\n";
+		return 1;
+	}
+	ofs << format("# Using %1% random points from r=%2% to %3%\n") % nR % rmin % rmax;
+	ofs << format("# Histogramming r from %1% to %2% in %3% bins\n") % r0 % r1 % nrbins;
+	ofs << format("# Histogramming mu from %1% to %2% in %3% bins\n") % mu0 % mu1 % nmubins;
 
 	// Now compute the histogram
 	Histogram2D h1(nrbins, r0, r1, nmubins, mu0, mu1);
-
-	cout << "# Starting histogram....\n";
 	double rr, mu;
-	for (int ii=0; ii < nR; ++ii)
-		for (int jj=0; jj < nR; ++jj) {
-			compute_rmu(pp[ii], pp[jj], rr, mu);
-			h1.add(rr, mu);
+
+	for (int isim=0; isim < nsims; ++isim) {
+		Pos pp = generate(nR, rmin, rmax, seed+isim);
+		for (int ii=0; ii < nR; ++ii)
+			for (int jj=0; jj < nR; ++jj) {
+				compute_rmu(pp[ii], pp[jj], rr, mu);
+				h1.add(rr, mu);
+			}
+	}
+
+	// Print out histogram
+	format histprint("%5i %5i %9.3f %9.3f %7.4f %7.4f %20.2f\n");
+	vector<paird> xr(nrbins), yr(nmubins);
+	for (int ii=0; ii < nrbins; ++ii) xr[ii] = h1.xrange(ii);
+	for (int ii=0; ii < nmubins; ++ii) yr[ii] = h1.yrange(ii);
+	for (int ii=0; ii < nrbins; ++ii) {
+		for (int jj=0; jj < nmubins; ++jj) {
+			ofs << histprint % ii % jj % get<0>(xr[ii]) % get<1>(xr[ii])
+					% get<0>(yr[jj]) % get<1>(yr[jj]) % h1(ii,jj);
 		}
-	cout << "# Histogram completed.....\n";
+	}
+
+	ofs.close();
 
 }
